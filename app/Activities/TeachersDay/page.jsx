5b5daf2c48cda4b4937d6217f1c7c9118a951f7d
kaavya <kaavyarao05@gmail.com>
2024-09-28 22:04:41 +0530

    feat: polaroid caption feat: added polaroid to teachers day

--- app/Activities/TeachersDay/page.jsx
+++ app/Activities/TeachersDay/page.jsx
@@ -17,7 +17,7 @@ const page = () => {
   return (
     <div className='bg-black top-0 z-10 min-w-full'>
         <div className="block h-fit">
-      <Image alt="Shine symbol outline" src={Shine} width={65} className='-hue-rotate-60 absolute mr-10 invert sm:right-[13%] -right-[1%] hover:scale-110 hover:rotate-45 transition:all translate-y-28'></Image>
+      <Image alt="Shine symbol outline" src={Shine} width={65} className='-hue-rotate-60 absolute mr-10 invert sm:right-[13%] -right-[1%] hover:scale-110 hover:rotate-45 translate-y-28'></Image>
       <Image alt="Shine symbol" src={ShineFill} width={26} className='-hue-rotate-60 absolute mr-10 invert sm:right-[10%] -right-[2%] hover:rotate-45 transition:all  translate-y-28'></Image>
       <Image alt="Shine symbol" src={ShineFill} width={80} className='-hue-rotate-60 absolute -ml-10 invert left-[20%] sm:left-[20%]  hover:scale-110 hover:rotate-45 translate-y-56'></Image>
       <Image alt="Shine symbol" src={ShineFill} width={30}  className='-hue-rotate-60 absolute -ml-10 invert left-[15%] sm:left-[20%] hover:rotate-45 transition:all  translate-y-56'></Image>
@@ -26,7 +26,7 @@ const page = () => {
         <h6 className="text-mustard -m-2 font-semibold font-mono">5 September 2024</h6>
       </div>
     </div>
-        <div className="grid gap-y-5 mx-6 py-5 m-auto justify-center">
+        <div className="grid gap-y-5 mx-10 py-5 m-auto justify-center">
             <Polaroid alt="Teachers Day" src={Img8} width={640} height={427} className={"rotate-3"}></Polaroid>
             <Polaroid alt="Teachers Day" src={Img7} width={640} height={427} className={"-rotate-3"}></Polaroid>
             <Polaroid alt="Teachers Day" src={Img6} width={640} height={427} className={"rotate-3"}></Polaroid>
--- app/components/polaroid.jsx
+++ app/components/polaroid.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
 
-const polaroid = ({src, width,height,alt,className,caption}) => {
+const polaroid = ({src, width,height,alt,className,caption="Placeholder Text"}) => {
   return (
     <div className={className}>
-    <Image 
-    className='border-white border-8'
-    src={src} width={width} height={height} alt={alt}></Image>
-    <h2 className='text-black -my-1 bg-white py-2 px-2 h-12'>{caption}</h2>
+        <div className='border-black duration-500 border-[0.5px] hover:scale-105 transition-all shadow-sm shadow-black shadow-'>
+        <Image 
+        className='border-white border-8'
+        src={src} width={width} height={height} alt={alt}></Image>
+        <h2 className='font-mono italic text-black -my-1 bg-white py-2 px-2 h-12'>{caption}</h2>
+        </div>
     </div>
-  )
+    )
 }
 
 export default polaroid
