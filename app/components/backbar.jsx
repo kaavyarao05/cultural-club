ec9907f472c0e099396e95b662a904e4cc247186
kaavya <kaavyarao05@gmail.com>
2024-09-28 22:24:47 +0530

    fix: backbar margin

--- app/components/backbar.jsx
+++ app/components/backbar.jsx
@@ -4,7 +4,7 @@ import SmallButton from './SmallButton'
 const Backbar = () => {
   return (
     <div className='bg-black'>
-        <div className='-my-2 text-right pr-3'>
+        <div className='-my-[5px] text-right pr-3'>
             <SmallButton text={"Go To Top"} href={"#Head"}></SmallButton>
         </div>
         <div className='w-full h-3 bg-wheat'>
